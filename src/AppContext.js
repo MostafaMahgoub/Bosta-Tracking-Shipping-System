b6ab035134a4867ab1caa0bf94522d30ebbd313a
MostafaR <mostafamahgoub@outlook.com>
2023-09-28 13:08:50 +0300

    Fetching and trackingNumber data and send it through to all the components inside the app

--- src/AppContext.js
+++ src/AppContext.js
@@ -1,17 +1,38 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect, useCallback } from 'react';
 
 export const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
   const [language, setLanguage] = useState('en');
+  const [trackingNumber, setTrackingNumber] = useState('');
+  const [data, setData] = useState(null);
 
   const toggleLanguage = () => {
     setLanguage(prevLanguage => (prevLanguage === 'en' ? 'ar' : 'en'));
   };
 
+  const fetchData = useCallback(async () => {
+    try {
+      const response = await fetch(`https://tracking.bosta.co/shipments/track/${trackingNumber}`);
+      const data = await response.json();
+      setData(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }, [trackingNumber]);
+
+  useEffect(() => {
+    if (trackingNumber) {
+      fetchData();
+    }
+  }, [trackingNumber, fetchData]);
+
   const contextValue = {
     language,
     toggleLanguage,
+    trackingNumber,
+    setTrackingNumber,
+    data,
   };
 
   return (
