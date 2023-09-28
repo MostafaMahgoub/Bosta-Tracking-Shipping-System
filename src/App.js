9e8aa4e641d60608698217d0bea0208e771efe1b
MostafaR <mostafamahgoub@outlook.com>
2023-09-28 19:00:34 +0300

    Update the rtl and ltr handler

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React from 'react';
-import { AppProvider } from './AppContext';
-import Header from './Header/Header';
+import React , {useContext} from 'react';
+import { AppProvider , AppContext } from './AppContext';
+import HeaderElement from './Header/Header';
 import ShippingProgress from './ShippingProgress/ShippingProgress';
 import ActivityLogs from './ActivityLogs/ActivityLogs';
 
 function App() {
   return (
     <AppProvider>
-      <Header />
-      <div className='flex sm:flex-col flex-row'>
-        <ShippingProgress />
-        <ActivityLogs />
-      </div>
+      <Content />
     </AppProvider>
+  );
+}
+
+function Content() {
+  const { language } = useContext(AppContext);
+
+  return (
+    <div className={`${language === 'en' ? 'ltr' : 'rtl'}`}>
+      <HeaderElement />
+    <div className="flex sm:flex-col flex-row">
+      <ShippingProgress />
+      <ActivityLogs />
+    </div>
+    </div>
   );
 }
 
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,10 +1,18 @@
 
+import { ReactComponent as BostaEnglish } from './assets/bosta-english.svg'
+import { ReactComponent as BostaArabic } from './assets/bosta-arabic.svg'
+import { AppContext } from '../AppContext';
+import React, { useContext } from 'react';
 
-function Header(){
+function HeaderElement(){
+    const { language } = useContext(AppContext);
+    
     return (
-        <></>
+        <header className='flex flex-row py-8 px-40'>
+            {language === 'en' ? <BostaEnglish /> : <BostaArabic />}
+        </header>
     )
 }
 
 
-export default Header;
+export default React.memo(HeaderElement);
