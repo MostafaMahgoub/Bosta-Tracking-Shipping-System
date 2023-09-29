2cee667f94a7bd37b85a7214f935f0842899a5c0
MostafaR <mostafamahgoub@outlook.com>
2023-09-29 16:45:05 +0300

    Styling fix

--- src/ActivityLogs/Components/LogsCard.js
+++ src/ActivityLogs/Components/LogsCard.js
@@ -2,7 +2,7 @@ import { Card } from "antd";
 
 function LogsCard({title,time}) {
   return (
-    <Card className="m-4" bodyStyle={{ padding: "0.5rem 1rem"  }}>
+    <Card className="m-4 border-[#dbdbdb]" bodyStyle={{ padding: "0.5rem 1rem"  }}>
       <p className="text-[#111619] m-0">{title}</p>
       <span className="text-[#667085]">{time}</span>
     </Card>
